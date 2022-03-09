f6131b95529bce7a5a28c8e12392244edce46341
brunosc10699 <brunosc10699@gmail.com>
2022-03-08 22:41:56 -0300

    course table customization

--- frontend/src/app/courses/courses.module.ts
+++ frontend/src/app/courses/courses.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
@@ -13,7 +13,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
